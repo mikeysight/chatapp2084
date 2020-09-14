a8f6f1914d945507db5c2cd4463d8687dca3c498
Michael Finn <mikeysight@gmail.com>
2020-09-14 13:05:58 -0700

    client progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import config from '/.config'
+import io from 'socket.io-client'
+
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+import BottomBar from './BottomBar'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      chat: [],
+      content: '',
+      name: '',
+    }
+  }
+
+  componentDidMount(){
+    this.socket = io(config[process.env.NODE_ENV].endpoint)
+
+    this.socket.on('init', (msg) => {
+      let msgReversed = msg.reverse()
+      this.setState((state) => ({
+      chat: [...state.chat, ...msgReversed],
+      }), this.scrollToBottom)
+    })
+    //update the chat if a new message is broadcasted
+    this.socket.on('push', (msg) => {
+      this.setState((state) => ({
+        chat: [...state.chat, msg],
+      }), this.scrollToBottom)
+    })
+  }
+
+  handleContent(event){
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  handleName(event){
+    this.setState()
+  }
 }
 
 export default App;
